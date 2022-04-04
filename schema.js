5a8b4d00e6ff79956b9151d491bc3a189b0bbb4c
ishitarastogi <ishita3099@gmail.com>
2022-04-04 21:25:49 +0530

    Updated schems.js

--- schema.js
+++ schema.js
@@ -2,48 +2,89 @@ const Joi = require("joi");
 const { ethers } = require("ethers");
 const addrPattern = /^0[xX][a-fA-F0-9]{40}$/;
 const expPattern = /^0[xX](?!0+$)[a-fA-F0-9]{40}$/;
+const multiplier = 60 * 60 * 1000;
+const abiJson = {};
 
 const schema = {
-  abis: Joi.object().keys({
-    monitorAbi: "",
+  accountBalance: Joi.object().keys({
+    developerAbbreviation: Joi.string().required(),
+    protocolName: Joi.string().optional(),
+    protocolAbbreviation: Joi.string().optional(),
+    address: Joi.string().pattern(new RegExp(addrPattern)).required(),
+    thresholdEth: Joi.number(),
+    alertMinimumIntervalSeconds: Joi.date().required(),
+    type: Joi.string().required(),
+    severity: Joi.string().required(),
   }),
   adminEvents: Joi.object().keys({
+    abiFile: Joi.object().required(),
     address: Joi.string().pattern(new RegExp(addrPattern)).required(),
-
-    expression: Joi.string().pattern(new RegExp(expPattern)).required(),
-
+    expression: Joi.any().when("address", {
+      is: expPattern,
+      then: Joi.required(),
+      otherwise: Joi.optional(),
+    }),
+    developerAbbreviation: Joi.string().required(),
+    protocolName: Joi.string().required(),
+    protocolAbbreviation: Joi.string().required(),
     proxy: Joi.string().alphanum().required(),
-  }),
-
-  accountBalance: Joi.object().keys({
-    address: Joi.string().pattern(new RegExp(addrPattern)).required(),
-    thresholdEth: Joi.number().valid(0),
-    alertMinimumIntervalSeconds: Joi.date()
-      .required()
-      .valid(Date.now() + 24 * 60 * 60 * 1000),
+    type: Joi.string().required(),
+    severity: Joi.string().required(),
   }),
   monitorFunctionCalls: Joi.object().keys({
+    abiFile: Joi.object().required(),
+    developerAbbreviation: Joi.string().required(),
+    protocolName: Joi.string().required(),
+    protocolAbbreviation: Joi.string().required(),
     address: Joi.string().pattern(new RegExp(addrPattern)).required(),
-    amount: Joi.number().integer().min(6000),
+    expression: Joi.any()
+      .when("address", {
+        is: expPattern,
+        then: Joi.required(),
+        otherwise: Joi.optional(),
+      })
+      .optional(),
+    type: Joi.string().required(),
+    severity: Joi.string().required(),
   }),
 };
 
-//Tested all of them, works fine
-// doubt in alertMinimumIntervalSeconds
-const zeroAddress = "0x0000000000000000000000000000000000000000";
-const address = "0x1aD91ee08f21bE3dE0BA2ba6918E714dA6B45836";
-const expression = "0x1aD91ee08f21bE3dE0BA2ba6918E714dA6B45836";
-const proxy = "contract1";
-const thresholdEth = 0;
-const amount = 6001;
-const wrongAmount = 5001;
-//let adminEventsData = { address, expression, proxy };
-// let adminEventsData = { address, zeroAddress, proxy }; gives error
-//let accountBalanceData = { address, thresholdEth };
-let monitorFunctionCalls = { address, amount };
-const result = schema.monitorFunctionCalls.validate(monitorFunctionCalls);
-// const result = schema.accountBalance.validate(accountBalanceData);
-// const result = schema.adminEvents.validate(adminEventsData);
+const accountBalance = {
+  developerAbbreviation: "",
+  protocolName: "",
+  protocolAbbreviation: "",
+  address: "",
+  thresholdEth: 0,
+  type: "Type",
+  severity: "Severity",
+  alertMinimumIntervalSeconds: 86400,
+};
+const adminEvents = {
+  developerAbbreviation: "",
+  protocolName: "",
+  protocolAbbreviation: "",
+  expression: expPattern,
+  type: "Type",
+  severity: "Severity",
+  address: "contractAddress1",
+  abiFile: "filename1.json",
+  proxy: "contractName2",
+};
+const monitorFunctionCalls = {
+  developerAbbreviation: "",
+  protocolName: "",
+  protocolAbbreviation: "",
+  address: "contractAddress1",
+  abiFile: "contractAbiFileName1.json",
+  expression: "7000",
+  type: "Type",
+  severity: "Severity",
+};
+
+const accountBalanceResult = schema.accountBalance.validate(accountBalance);
+const adminEventsResult = schema.adminEvents.validate(adminEvents);
+const monitorFunctionCallsResult =
+  schema.monitorFunctionCalls.validate(monitorFunctionCalls);
 
 if (result.error) {
   console.log(result.error.details);
